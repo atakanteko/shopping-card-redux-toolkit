939fdc5c86079a46d08c40ca0836e75b5dfd03a6
atakantekoglu <atakantekoglu@gmail.com>
2022-08-11 16:28:32 +0300

    Create a Redux State Slice

--- src/store.js
+++ src/store.js
@@ -1,5 +1,8 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { configureStore } from '@reduxjs/toolkit';
+import cardReducer from './features/card/cardSlice';
 
 export const store = configureStore({
-    reducer: {},
+    reducer: {
+        cart: cardReducer,
+    },
 })
